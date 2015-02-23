aaecf60f3bc2751ac0abc2c192199fdbb179111c
Mireia <mireia@mireiasangalo.com>
2015-02-23 16:05:33 +0000

    Initial commit

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by My Pitit on 06/02/2015.
+//  Copyright (c) 2015 Ukon Cherry. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+    }
+
+}
